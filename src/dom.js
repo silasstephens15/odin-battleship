3ae068ab1560677f22a8410f00c84a161be6cd38
Silas Stephens <76085257+silasstephens15@users.noreply.github.com>
2025-11-07 21:37:41 -0500

    feat: added rendering for ships.

--- src/dom.js
+++ src/dom.js
@@ -4,7 +4,7 @@ function generateGameBoard(player) {
   for (let i = 0; i < player.gameBoard.size; i++) {
     for (let j = 0; j < player.gameBoard.size; j++) {
       const tile = document.createElement("div");
-      tile.id = `${j},${i}`;
+      tile.id = `a${j}-${i}`;
       tile.classList.add("tile");
       if (player.isComputer) {
         tile.classList.add("computer-tile");
@@ -16,4 +16,15 @@ function generateGameBoard(player) {
   container.appendChild(board);
 }
 
-export { generateGameBoard };
+function renderShips(ships) {
+  for (let i = 0; i < ships.length; i++) {
+    for (let j = 0; j < ships[i].length; j++) {
+      const tile = document.querySelector(
+        `#a${ships[i][j][0]}-${ships[i][j][1]}`
+      );
+      tile.style.backgroundColor = "blue";
+    }
+  }
+}
+
+export { generateGameBoard, renderShips };
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import "./styles.css";
 import { Player } from "./game.js";
-import { generateGameBoard } from "./dom.js";
+import { generateGameBoard, renderShips } from "./dom.js";
 
 let playing = false;
 
@@ -12,10 +12,22 @@ generateGameBoard(player2);
 
 const boardContainer = document.querySelector("#board-container");
 const randomButton = document.querySelector("button");
-randomButton.addEventListener("click", () => {});
+randomButton.addEventListener("click", () => {
+  player1.gameBoard.randomizeShips(2, 3, 4, 4, 5);
+  const shipTiles = [];
+  for (let i = 0; i < player1.gameBoard.ships.length; i++) {
+    shipTiles.push(player1.gameBoard.ships[i].allPos);
+  }
+  renderShips(shipTiles);
+});
 boardContainer.addEventListener("click", (e) => {
   if (e.target.classList.contains("computer-tile")) {
-    if (player2.gameBoard.receiveAttack(JSON.parse(`[${e.target.id}]`))) {
+    if (
+      player2.gameBoard.receiveAttack([
+        Number(e.target.id.slice(1, 1)),
+        Number(e.target.id.slice(3, 3)),
+      ])
+    ) {
       e.target.classList.add("hit");
     } else {
       e.target.classList.add("miss");
